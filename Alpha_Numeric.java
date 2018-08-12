f6f702606bacae66fe0c751f7f8fc7d8bc5c1d00
Aravind Babu S <36079750+A-r-v-i@users.noreply.github.com>
2018-08-12 18:47:50 +0530

    AlphaNumeric java Program

---
+++ Alpha_Numeric.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+public class Alpha_Numeric {
+	public static void main(String args[])
+	{
+		Scanner sc=new Scanner(System.in);
+	    String in=sc.next();
+	    String num=in.replaceAll("[^0-9]","");
+	   
+	    String number[]=num.split("\\s+");
+	    int  sum=0;
+	    for(String temp:number)
+	    {
+	    	Integer n=Integer.parseInt(temp);
+	    	if(n!=0)
+	    	{
+	    		int a=n%10;
+	    		n=n/10;
+	    		sum=sum+a;
+	    	}
+	    }
+	}
+
+}
